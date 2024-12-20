1021a1fa50a283ff040865169114702461478218
MinhDuy <dennisnguyen0909@gmail.com>
2024-12-20 17:47:56 +0700

    fix-bug-validate-publicId

--- src/controllers/CloudinaryController.js
+++ src/controllers/CloudinaryController.js
@@ -2,6 +2,9 @@ const cloudinary = require("cloudinary");
 const deleteCloudinaryImage = require("../services/CloudinaryService");
 const deleteImage = async (req, res) => {
   const publicId = req.params.id;
+  if (publicId === "" || !publicId) {
+    es.status(500).send("publicId not found!");
+  }
   try {
     await deleteCloudinaryImage(publicId);
     res.status(200).send("Image deleted successfully");
