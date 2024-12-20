fd03d2b8dc8a0dc063a78af025a317ffa10375eb
MinhDuy <dennisnguyen0909@gmail.com>
2024-12-20 18:01:23 +0700

    completed api get all user

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -10,6 +10,8 @@ const UserRouter = (app) => {
   router.post("/login", UserController.loginUser);
   router.put("/update-user/:id", authUserMiddleware, UserController.updateUser);
   router.delete("/delete-user/:id", authMiddleware, UserController.deleteUser);
+  router.get("/getAll", authMiddleware, UserController.getAllUser);
+  router.get("/getAllUser", authMiddleware, UserController.getAllUser);
   return app.use("/api/user", router);
 };
 
