2422b75b7c56b2de2141fc971c3ad43c099c819e
MinhDuy <dennisnguyen0909@gmail.com>
2024-12-20 17:55:38 +0700

    api-create-user

---
+++ src/routes/UserRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const UserController = require("../controllers/UserController");
+const {
+  authMiddleware,
+  authUserMiddleware,
+} = require("../middleware/authMiddleware");
+const router = express.Router();
+const UserRouter = (app) => {
+  router.post("/sign-up", UserController.createUser);
+  return app.use("/api/user", router);
+};
+
+module.exports = UserRouter;
