07a3ecbf4393b73213bda0f57cb07e25a89f7a6e
MinhDuy <dennisnguyen0909@gmail.com>
2024-12-20 18:28:55 +0700

    create api get all

--- src/controllers/ShopController.js
+++ src/controllers/ShopController.js
@@ -14,4 +14,17 @@ const createNameShop = async (req, res) => {
   }
 };
 
-module.exports = { createNameShop };
+const getAllShop = async (req, res) => {
+  try {
+    console.log("đã vô");
+    const response = await ShopService.getAllShop();
+    console.log("res", response);
+    return res.status(200).json({ response });
+  } catch (e) {
+    return res.status(500).json({
+      EM: "Error",
+    });
+  }
+};
+
+module.exports = { createNameShop, getAllShop };
